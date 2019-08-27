c9672667178a2eef8d8418d3246f4d7be754ffb6
Mihir Pathak <mihirpathak97@users.noreply.github.com>
2019-08-27 18:48:28 +0530

    Add bare bones support for data and filters

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-jquery-datatable'
+import ReactTable from 'react-table'
 
 export default class App extends Component {
   render () {
+
+    const data = [
+      {
+        name: 'Mihir',
+        email: 'mihirpathak97',
+        shouldShow: '1'
+      },
+      {
+        name: 'Bleh',
+        email: 'mihirpathak',
+        shouldShow: '0'
+      }
+    ]
+
+    const columns = [
+      {
+        key: 'name',
+        title: 'Name'
+      },
+      {
+        key: 'email',
+        title: 'E-mail'
+      },
+      {
+        key: 'shouldShow',
+        title: 'Should Show'
+      },
+    ]
+
+    const filters = [
+      {
+        type: 'select',
+        title: 'Should Show?',
+        data: ['Mihir', 'Bleh'],
+        key: 'name'
+      }
+    ]
+
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ReactTable columns={columns} data={data} filters={filters} />
       </div>
     )
   }
